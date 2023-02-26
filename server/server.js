e60b2f027aab20dcf4963570f375ad34acd0533f
bsande6 <bsande6@g.clemson.eu>
2023-02-26 13:15:34 -0500

    user model

--- server/server.js
+++ server/server.js
@@ -1,33 +1,55 @@
 const {MongoClient} = require('mongodb');
 
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require('mongoose');
+const bodyParser  = require('body-parser');
+const users = require('./routes/user');
 
-  async function main() {
-  require("dotenv").config({ path: "./config.env" });
-  uri = process.env.ATLAS_URI
-  const client = new MongoClient(uri);
+mongoose.connect('mongodb://localhost/mongo-games')
+    .then(() => console.log('Now connected to MongoDB!'))
+    .catch(err => console.error('Something went wrong', err));
+    
+//Routes
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-  try {
-    await client.connect();
+app.use(cors());
+app.use(express.json());
 
-    await listDatabases(client);
+app.get('/message', (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
 
-  } catch (e) {
-    console.error(e);
-  }
+app.listen(5000, () => {
+  console.log(`Server is running on port 8000.`);
+});
 
-  finally {
-    await client.close();
-  }
-}
+// async function main() {
+//   require("dotenv").config({ path: "./config.env" });
+//   uri = process.env.ATLAS_URI
+//   const client = new MongoClient(uri);
 
-main().catch(console.error);
+//   try {
+//     await client.connect();
 
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
+//     await listDatabases(client);
 
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+//   } catch (e) {
+//     console.error(e);
+//   }
+
+//   finally {
+//     await client.close();
+//   }
+// }
+
+// main().catch(console.error);
+
+// async function listDatabases(client){
+//   databasesList = await client.db().admin().listDatabases();
+
+//   console.log("Databases:");
+//   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
